db605cd1f601197fc795abb5bae245d2c5a72c73
Ludovic16513 <56023526+Ludovic16513@users.noreply.github.com>
2019-11-26 16:15:55 +0100

    Version 1.0

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 var choix1 = document.getElementById('item');
 var choix2 = document.getElementById('item2');
 var choix3 = document.getElementById('item3');
+var bouton = document.getElementById('bouton');
+var Cyriak = document.getElementById('cyriakdiv');
 
 var nul = 0;
 var victoire = 0;
@@ -8,6 +10,7 @@ var defaite = 0;
 var valid = 0;
 var IA1 = document.getElementById('IA1');
 
+Cyriak.style.background = 'url("Cyriak.png")';
 
 choix1.addEventListener("click", function () {
     valid = 1;
@@ -29,30 +32,38 @@ choix1.addEventListener("click", function () {
     else if  ( valid === 1 && alea === 3){
         compteurdef.innerHTML = 'COUPS SUBIS ' +defaite ;
         defaite = defaite +1;
-
-
     }
-
-
-
-    console.log(alea);
-    console.log(victoire);
-
 
     switch (alea) {
         case 1: {
-            IA1.style.background = 'url("Rock-paper-scissors_(paper).png")';
+            IA1.style.background = 'url("feuille.png")';
             break;
         }
         case 2: {
-            IA1.style.background = 'url("Rock-paper-scissors_(rock).png")';
+            IA1.style.background = 'url("pierre.png")';
             break;
         }
         case 3: {
-            IA1.style.background = 'url("Rock-paper-scissors_(scissors).png")';
+            IA1.style.background = 'url("ciseau.png")';
             break;
         }
     }
+
+    switch (defaite) {
+        case 5 :
+            Cyriak.style.background = 'url("Cyriak1.png")';
+            break;
+        case 10 :
+            Cyriak.style.background = 'url("Cyriak2.png")';
+            break;
+        case 15 :
+            Cyriak.style.background = 'url("Cyriak3.png")';
+            break;
+        case 20 :
+            Cyriak.style.background = 'url("Cyriak4.png")';
+            break;
+    }
+
 
 });
 
@@ -62,6 +73,8 @@ choix2.addEventListener("click", function () {
     var compteurvic = document.getElementById('victoire');
     var compteurdef = document.getElementById('defaite');
     var compteurnul = document.getElementById('nul');
+
+
 
     if ( valid === 2 && alea === 3){
         compteurvic.innerHTML = 'COUPS ENVOYES ' +victoire ;
@@ -79,23 +92,33 @@ choix2.addEventListener("click", function () {
         defaite = defaite +1;
     }
 
-    console.log(alea);
-    console.log(victoire);
-
-
     switch (alea) {
         case 1: {
-            IA1.style.background = 'url("Rock-paper-scissors_(paper).png")';
+            IA1.style.background = 'url("FEUILLE.png")';
             break;
         }
         case 2: {
-            IA1.style.background = 'url("Rock-paper-scissors_(rock).png")';
+            IA1.style.background = 'url("pierre.png")';
             break;
         }
         case 3: {
-            IA1.style.background = 'url("Rock-paper-scissors_(scissors).png")';
+            IA1.style.background = 'url("ciseau.png")';
             break;
         }
+    }
+    switch (defaite) {
+        case 5 :
+            Cyriak.style.background = 'url("Cyriak1.png")';
+            break;
+        case 10 :
+            Cyriak.style.background = 'url("Cyriak2.png")';
+            break;
+        case 15 :
+            Cyriak.style.background = 'url("Cyriak3.png")';
+            break;
+        case 20 :
+            Cyriak.style.background = 'url("Cyriak4.png")';
+            break;
     }
 
 });
@@ -107,6 +130,7 @@ choix3.addEventListener("click", function () {
     var compteurvic = document.getElementById('victoire');
     var compteurdef = document.getElementById('defaite');
     var compteurnul = document.getElementById('nul');
+
 
     if (valid === 3 && alea === 1) {
         compteurvic.innerHTML = 'COUPS ENVOYES ' +victoire ;
@@ -123,22 +147,47 @@ choix3.addEventListener("click", function () {
         defaite = defaite +1;
     }
 
-    console.log(alea);
-    console.log(victoire);
-
     switch (alea) {
         case 1: {
-            IA1.style.background = 'url("Rock-paper-scissors_(paper).png")';
+            IA1.style.background = 'FEUILLE.png")';
             break;
         }
         case 2: {
-            IA1.style.background = 'url("Rock-paper-scissors_(rock).png")';
+            IA1.style.background = 'url("pierre.png")';
             break;
         }
         case 3: {
-            IA1.style.background = 'url("Rock-paper-scissors_(scissors).png")';
+            IA1.style.background = 'url("ciseau.png")';
             break;
         }
     }
+    switch (defaite) {
+        case 5 :
+            Cyriak.style.background = 'url("Cyriak1.png")';
+            break;
+        case 10 :
+            Cyriak.style.background = 'url("Cyriak2.png")';
+            break;
+        case 15 :
+            Cyriak.style.background = 'url("Cyriak3.png")';
+            break;
+        case 20 :
+            Cyriak.style.background = 'url("Cyriak4.png")';
+            break;
+    }
 
+});
+
+bouton.addEventListener("click", function () {
+    var compteurvic = document.getElementById('victoire');
+    var compteurdef = document.getElementById('defaite');
+    var compteurnul = document.getElementById('nul');
+    nul = 0;
+    victoire = 0;
+    defaite = 0;
+    valid = 0;
+    Cyriak.style.background = 'url("Cyriak.png")';
+    compteurvic.innerHTML = 'COUPS ENVOYES ' +victoire ;
+    compteurdef.innerHTML = 'COUPS SUBIS ' +defaite ;
+    compteurnul.innerHTML = 'PARADE ' +nul ;
 });
